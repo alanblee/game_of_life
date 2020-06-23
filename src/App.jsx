224cfb5627c96d52eeac6049f6578deaf0aac3bd
alanblee <alanbenlee12@gmail.com>
2020-06-22 21:12:24 -0700

    setting up files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <h1>Game of Life</h1>
+    </div>
+  );
 }
 
 export default App;
