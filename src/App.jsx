614263e390308fdaf5a985f06cbb28ba7683eb22
alanblee <alanbenlee12@gmail.com>
2020-06-23 12:44:22 -0700

    setting up state for grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
+import Grid from "./components/grid/grid";
 import "./App.scss";
 
 const App = (generations) => {
+  const [speed, setSpeed] = useState(100);
+  const [rows, setRows] = useState(30);
+  const [cols, setCols] = useState(50);
+
+  useEffect(() => {}, []);
   return (
     <div className="">
       <h1>Game of Life</h1>
       <h2>Generations: {generations.generations} </h2>
+      <Grid />
     </div>
   );
 };
--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -1,1 +1,9 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+
+const Grid = () => {
+  return <h1>Grid</h1>;
+};
+
+export default compose(connect(null, null))(Grid);
